43223860f62fb8f14844b2ea065e51d23145d366
Matthew Bishop <mattsbishop@gmail.com>
2011-01-10 16:58:41 -0700

    Fixed output formatting for \!update

--- lib/rubybot2/bin/update.rb
+++ lib/rubybot2/bin/update.rb
@@ -5,7 +5,7 @@ SYNTAX = 'Usage: !update'
 def handle_command(nick, dest, args)
   git = `git pull origin HEAD`
   out = ''
-  git.each_line { |line| out << "P\t#{line}\n" }
+  git.each_line { |line| out << "P\t#{line.strip}\n" }
   out
 end
 
