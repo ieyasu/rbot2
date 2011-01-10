9e4ff6e6f4ad204182cdce7a273f3e87d65610a7
Matthew Bishop <mattsbishop@gmail.com>
2011-01-10 16:30:20 -0700

    Added git updater command

---
+++ lib/rubybot2/bin/update.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+SYNTAX = 'Usage: !update'
+
+def handle_command(nick, dest, args)
+  git = `git pull origin HEAD`
+  out = ''
+  git.each_line { |line| out << "P\t#{line}\n" }
+  out
+end
+
+load 'boilerplate.rb'
