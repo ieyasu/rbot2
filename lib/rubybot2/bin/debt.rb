e60af8e36e49252d7e1a6586dbad755575990a31
Tony Arcieri <tony.arcieri@gmail.com>
2011-06-07 23:50:32 -0600

    Convert !debt to Nokogiri

--- lib/rubybot2/bin/debt.rb
+++ lib/rubybot2/bin/debt.rb
@@ -1,11 +1,12 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'hpricot'
+require 'nokogiri'
 require 'open-uri'
 
 def handle_command(nick, dest, args)
-    debt = Hpricot(open('http://brillig.com/debt_clock')).at('img')['alt'].gsub(' ', '')
+    doc = Nokogiri::HTML(open('http://brillig.com/debt_clock').read)
+    debt = doc.xpath('//img').first[:alt].gsub(/\s/, '')
     "P\tUS National Debt is: #{debt}"
 end
 
