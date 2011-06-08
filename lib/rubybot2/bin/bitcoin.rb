3468989df654309f1bccf2dd42435bca664a7009
Tony Arcieri <tony.arcieri@gmail.com>
2011-06-08 14:27:38 -0600

    Show buy/sell prices and volume instead of high/low

--- lib/rubybot2/bin/bitcoin.rb
+++ lib/rubybot2/bin/bitcoin.rb
@@ -6,12 +6,13 @@ require 'json'
 
 def handle_command(nick, dest, args)
   data = JSON.parse(open("https://mtgox.com/code/data/ticker.php").read)['ticker']
-  
-  last = "%0.2f" % data['last']
-  high = "%0.2f" % data['high']
-  low  = "%0.2f" % data['low']
-  
-  "P\tMt. Gox: $#{last} (high: $#{high}, low: $#{low})"
+
+  last = "$%0.2f" % data['last']
+  prices = %w(buy sell).map do |price|
+    "#{price}: $%0.2f" % data[price]
+  end.join(" ")
+
+  "P\tMt. Gox: #{last} #{prices}, vol: #{data['vol']} BTC"
 end
 
 load 'boilerplate.rb'
