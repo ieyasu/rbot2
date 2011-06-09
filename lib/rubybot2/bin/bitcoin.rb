dff081de86918c3036414806a192b157286af3fe
Tony Arcieri <tony.arcieri@gmail.com>
2011-06-08 18:20:37 -0600

    Output cleanup

--- lib/rubybot2/bin/bitcoin.rb
+++ lib/rubybot2/bin/bitcoin.rb
@@ -10,9 +10,9 @@ def handle_command(nick, dest, args)
   last = "$%0.2f" % data['last']
   prices = %w(buy sell).map do |price|
     "#{price}: $%0.2f" % data[price]
-  end.join(" ")
+  end.join(", ")
 
-  "P\tMt. Gox: #{last} #{prices}, vol: #{data['vol']} BTC"
+  "P\tMt. Gox: #{last} -- #{prices}, vol: #{data['vol']} BTC"
 end
 
 load 'boilerplate.rb'
