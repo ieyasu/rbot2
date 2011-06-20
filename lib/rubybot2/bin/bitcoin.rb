dadcbbdc1145c0229a7b8c8879ad2909bff08b7a
Tony Arcieri <tony.arcieri@gmail.com>
2011-06-20 17:18:26 -0600

    Weighted price

--- lib/rubybot2/bin/bitcoin.rb
+++ lib/rubybot2/bin/bitcoin.rb
@@ -3,6 +3,7 @@
 require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 def handle_command(nick, dest, args)
    doc = Nokogiri::HTML(open("https://www.tradehill.com/MarketData/") { |response| response.read })
@@ -10,7 +11,9 @@ def handle_command(nick, dest, args)
    match = data.match(/Last: (\d+.\d+) USD, .+Highest Bid: (\d+.\d+)Lowest Ask: (\d+.\d+)/)
    last, bid, ask = [1,2,3].map { |n| sprintf("$%0.2f", Float(match[n])) }
 
-  "P\tTradeHill: #{last} -- bid: #{bid}, ask: #{ask}"
+   weighted = Float(JSON.parse(open("http://bitcoincharts.com/t/weighted_prices.json").read)["USD"]["24h"])
+
+  "P\tTradeHill: #{last} -- bid: #{bid}, ask: #{ask}, weighted: #{sprintf("$%0.2f", weighted)}"
 end
 
 load 'boilerplate.rb'
