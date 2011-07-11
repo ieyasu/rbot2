e1b7c8d8db3a0f287ff2ec11d2b71601ff689855
Matthew Bishop <mattsbishop@gmail.com>
2011-07-11 11:31:34 -0600

    grab imgur titles

--- lib/rubybot2/plugins/title_grabber.rb
+++ lib/rubybot2/plugins/title_grabber.rb
@@ -5,27 +5,31 @@ require 'open-uri'
 # Grabs titles from web pages with non-useful URLs and displays them in
 # the originating channel. Just youtube for now.
 class TitleGrabber
-    include Web
+  include Web
 
-    def initialize(client)
-        @client = client
-        @channels = []
-        @janitor = ThreadJanitor.new
-    end
+  def initialize(client)
+    @client = client
+    @channels = []
+    @janitor = ThreadJanitor.new
+  end
 
-    def m_PRIVMSG(msg, replier)
-        if msg.text =~ %r!((?:https?://)?(?:\w+\.)?youtube\.com/watch[^ \t>)]+)!
-            url = $1
-            @janitor.register(Thread.new { fetch_title(url, replier) })
-        end
-    end
+  GRAB_URLS = [%r!((?:https?://)?(?:\w+\.)?youtube\.com/watch[^ \t>)]+)!,
+               %r!((?:https?://)?(?:i\.)?imgur\.com/(?:gallery/)?[\w\d]+)(?:\.(?:jpe?g|png))?!i]
 
-    def fetch_title(url, replier)
-        body = open(url).read
-        i = body.index('<title') or return
-        j = body.index('</title', i) or return
-        replier.reply("Title: #{strip_html(body[i...j])}")
-    rescue Exception => e
-        @client.logger.warn("!!! exception fetching #{url}: #{e.inspect}: #{e.message} #{e.backtrace.join("\n")}")
+  def m_PRIVMSG(msg, replier)
+    if GRAB_URLS.any? {|url| msg.text =~ url}
+      url = $1
+      puts "url = #{url.inspect}"
+      @janitor.register(Thread.new { fetch_title(url, replier) })
     end
+  end
+
+  def fetch_title(url, replier)
+    body = open(url).read
+    i = body.index('<title') or return
+    j = body.index('</title', i) or return
+    replier.reply("Title: #{strip_html(body[i...j])}")
+  rescue Exception => e
+    @client.logger.warn("!!! exception fetching #{url}: #{e.inspect}: #{e.message} #{e.backtrace.join("\n")}")
+  end
 end
