e84dd6d4bb1f26493c78d29659701ff0baee4be3
Tony Arcieri <tony.arcieri@gmail.com>
2011-07-25 20:54:30 -0700

    Fix TradeHill scraping

--- lib/rubybot2/bin/bitcoin.rb
+++ lib/rubybot2/bin/bitcoin.rb
@@ -6,12 +6,12 @@ require 'nokogiri'
 require 'json'
 
 def handle_command(nick, dest, args)
-   doc = Nokogiri::HTML(open("https://www.tradehill.com/MarketData/") { |response| response.read })
-   data = doc.xpath("/html/body/div[4]/fieldset/div/table/tr/td").text
-   match = data.match(/Last: (\d+.\d+) USD, .+Highest Bid: (\d+.\d+)Lowest Ask: (\d+.\d+)/)
-   last, bid, ask = [1,2,3].map { |n| sprintf("$%0.2f", Float(match[n])) }
+  doc = Nokogiri::HTML(open("https://www.tradehill.com/MarketData/") { |response| response.read })
+  data = doc.css("div#ticker2").xpath('table/tr/td').text
+  match = data.match(/Last: (\d+.\d+) USD, .+Highest Bid: (\d+.\d+)Lowest Ask: (\d+.\d+)/)
+  last, bid, ask = [1,2,3].map { |n| sprintf("$%0.2f", Float(match[n])) }
 
-   weighted = Float(JSON.parse(open("http://bitcoincharts.com/t/weighted_prices.json").read)["USD"]["24h"])
+  weighted = Float(JSON.parse(open("http://bitcoincharts.com/t/weighted_prices.json").read)["USD"]["24h"])
 
   "P\tTradeHill: #{last} -- bid: #{bid}, ask: #{ask}, 24h weighted: #{sprintf("$%0.2f", weighted)}"
 end
