816cea235694a889dab7742ff2ccab013e581fb6
sargon <sargon@bigfeh.bigevilcorp.com>
2011-08-04 11:27:34 -0600

    Update unmd5 to handle the daily limit

--- lib/rubybot2/bin/unmd5.php
+++ lib/rubybot2/bin/unmd5.php
@@ -4,11 +4,12 @@
 	if(strlen($args) < 1) reply_exit("Give me an MD5 string and I'll try to reverse it");
 	try {
 		@$result = file_get_contents('http://www.md5decrypter.co.uk/feed/api.aspx?' . urlencode($args));
+		if($result == "") reply_exit("Error: 50-hash daily limit reached?");
+		$xml = new SimpleXMLElement($result);
+		$result = $xml->hash->text;
+		if($result == "") reply_exit("Empty string");
+		reply_exit($result);
 	} catch (Exception $e) {
-		reply_exit("Not found");
+		reply_exit("Not found: $e");
 	}
-	$xml = new SimpleXMLElement($result);
-	$result = $xml->hash->text;
-	if($result == "") reply_exit("Not found");
-	reply_exit($result);
 ?>
