f484978b8ac28b71454a9ae0bbbbe5109de18f6e
Matthew Bishop <mattsbishop@gmail.com>
2012-01-12 17:23:32 -0700

    doesn't seem to work anymore

--- web/cb/wtemp.php
+++
@@ -1,9 +0,0 @@
-<?
-	$src = file("http://www.weather.com/weather/local/$args");
-	foreach($src as $line) {
-		if(preg_match("/OAS_query = '(.*)'/", $line, $matches)) {
-			$data = split("&", $matches[1]);
-			print_r($data);
-		}
-	}
-?>
