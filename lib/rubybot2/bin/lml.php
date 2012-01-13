4e2c674e1ddf658dbfaaa02e0312a7ff490b0398
Matthew Bishop <mattsbishop@gmail.com>
2012-01-13 12:04:43 -0700

    make a proper command

--- lib/rubybot2/bin/lml.php
+++ lib/rubybot2/bin/lml.php
@@ -1,10 +1,11 @@
-<?php
+#!/usr/bin/php
+<?
+require_once("boilerplate.php");
 
 $content = file_get_contents("http://www.lmylife.com/?sort=random");
 
 preg_match_all('/<p>.*<\/p>/',$content,$fmls);
 
 $random = $fmls[0][rand(0,count($fmls[0])-1)];
-print(trim(stripslashes(strip_tags($random))));
-
+reply(trim(stripslashes(strip_tags($random))));
 ?>
