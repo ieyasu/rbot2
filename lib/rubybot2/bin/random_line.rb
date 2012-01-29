9a6365521268c082363c132bb5386b8f1749f633
Matthew Bishop <mattsbishop@gmail.com>
2012-01-29 01:39:04 -0700

    Use fisher-yates shuffle in a separate file for better 'randomness'

--- lib/rubybot2/bin/random_line.rb
+++ lib/rubybot2/bin/random_line.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-FILE_MAP = {
+HOOK_TO_FILE = {
   'gq' => 'galenquotes',
   'maruth' => 'maruth.txt',
   'dec' => 'dec.txt',
@@ -50,29 +50,56 @@ FLAGS = {
   'deepdick' => 'na'
 }
 
+# Does a fish-yates shuffle on the lines from infile, writing to outfile
+def shuffle_file(infile, outfile)
+  lines = File.open(infile) {|fin| fin.readlines}
+  j = lines.length - 1
+  begin
+    k = rand(lines.length)
+    l = lines[k]
+    lines[k] = lines[j]
+    lines[j] = l
+    j -= 1
+  end while j > 0
+  File.open(outfile, 'w') {|fout| fout.puts(lines)}
+end
+
+def random_line(file)
+  random_file = "#{file}-rand"
+  unless File.exist?(random_file)
+    shuffle_file(file, random_file)
+  end
+
+  # grab and delete last line of file
+  File.open(random_file, 'r+') do |fin|
+    text = fin.read
+    i = text.rindex("\n", text.length - 2)
+    j = i ? i + 1 : 0
+    last_line = text[j..-1].rstrip
+    if i
+      fin.truncate(text[0..i].bytesize)
+    else
+      File.delete(random_file)
+    end
+    last_line
+  end
+end
+
 def handle_command(nick, dest, args)
   # figure out which !command this is
   command = File.basename($0)
-
-  return "P\tNo file mapping for #{command}!" unless FILE_MAP[command]
-
-  # select random line from file
-  random_line = nil
-  IO.readlines('db/' + FILE_MAP[command]).each_with_index do |line, i|
-    next if line.length < 3
-    random_line = line if rand(i + 1) < 1
-  end
-  random_line = random_line.chop
+  file = HOOK_TO_FILE[command]
+  return "P\tNo file mapping for #{command}!" unless file
+  rl = random_line("db/#{file}")
 
   if FLAGS[command] && FLAGS[command].index('n')
-    random_line = random_line.gsub('$args', args).gsub('$source', nick)
+    rl = rl.gsub('$args', args).gsub('$source', nick)
   end
 
-  # print reply
   if FLAGS[command] && FLAGS[command].index('a')
-    "P\t\001ACTION #{random_line}\001"
+    "P\t\001ACTION #{rl}\001"
   else
-    "P\t#{random_line}"
+    "P\t#{rl}"
   end
 end
 
