80c87dcc4439de1a5ca76f50dae0ef76c286ca3d
Matthew Bishop <mattsbishop@gmail.com>
2012-01-29 11:26:38 -0700

    fix encoding issues a little better this time

--- lib/rubybot2/bin/calc.rb
+++ lib/rubybot2/bin/calc.rb
@@ -2,14 +2,9 @@
 
 SYNTAX = 'Usage: !calc <expression>'
 
-def comify(str)
-  str.gsub("\240", ',')
-end
-
 def googlecalc(expr)
   data = {}
   open("http://www.google.com/ig/calculator?hl=en&q=#{CGI.escape(expr)}").read.
-    force_encoding("ASCII-8BIT").
     scan(/(\w+): "((?:[^"]|\\")*)"/).
     each {|ary| data[ary.first] = ary.last }
 
@@ -21,10 +16,10 @@ def googlecalc(expr)
     else
       "#{data['lhs']} = #{data['rhs']}"
     end
-  result = comify result.gsub(/\\x([0-9a-fA-F]{2})/) {|m| $1.hex.chr}.
+  result.encode("UTF-8").
+    gsub(/\\x([0-9a-fA-F]{2})/) {|m| $1.hex.chr}.
     gsub(/&#([0-9]{1,7});/) {[$1.to_i].pack('U')}.
     gsub(/<sup>(-?\d+)<\/sup>/) {"^#{$1}"}
-  result.encode("UTF-8")
 end
 
 def handle_command(nick, dest, args)
