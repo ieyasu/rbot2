ccfe4bdf715b9468eadc511dc62a2025fe063415
Matthew Bishop <mattsbishop@gmail.com>
2012-03-23 17:02:01 -0600

    replacement \!domain written in ruby parsing output from whois command

---
+++ lib/rubybot2/bin/domain.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'shellwords'
+
+def handle_command(nick, dest, args)
+  resp = `whois -H -F '#{Shellwords.shellescape args}'`
+
+  if resp =~ /^\s*Expiration Date:\s*(.*)/
+    "P\t#{args} expires #{$1}"
+  elsif resp =~ /No match for/
+    "P\t#{args} not found"
+  else
+    "P\tParse error in whois response for #{args}"
+  end
+end
+
+load 'boilerplate.rb'
