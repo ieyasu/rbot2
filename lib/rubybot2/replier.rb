4a095ad363af22e35386eba74ae463152f909c18
Matthew Bishop <bishop@atmos.colostate.edu>
2012-03-27 00:23:48 -0600

    fix Replier to work with stdout

--- lib/rubybot2/replier.rb
+++ lib/rubybot2/replier.rb
@@ -1,98 +1,82 @@
 require 'rubybot2/irc'
-require 'thread'
 
 class IRC::TooManyLines < Exception; end
 
-# Given an original request message and an irc client, allows client code
-# high level access for responding to the request in different ways.
-# There are a maximum number of allowed lines (IRC messages), different
-# counts for public and private messages.
-class IRC::Replier
-    SOFT_PUBLIC_LIMIT = 4
-    SOFT_PRIVATE_LIMIT = 12
+STDOUT.sync = true # don't need to call flush
 
-    attr_accessor :defdest
+# Given an original request message, provides high level interface for
+# sending IRC messages of various kinds, public or private.  There are
+# a maximum number of allowed lines (IRC messages), different counts
+# for public and private messages.
+class Replier
+  SOFT_PUBLIC_LIMIT = 4
+  SOFT_PRIVATE_LIMIT = 12
 
-    # Initializes the Replier from the given IRC client and request message.
-    # Determines the default destination for a reply: if a message was sent
-    # to a channel, then the reply will go to that channel by default; if
-    # a message was sent to the bot, the reply will go to the sender directly.
-    def initialize(client, msg)
-        @client = client
-        @msg = msg
-        @public_lines = 0
-        @private_lines = 0
-        @defdest = msg.reply_to if msg.respond_to?(:reply_to)
-        @@write_lock ||= Mutex.new
+  attr_accessor :default_dest
+
+  # Initializes the Replier from the given IRC client and request message.
+  # Determines the default destination for a reply: if a message was sent
+  # to a channel, then the reply will go to that channel by default; if
+  # a message was sent to the bot, the reply will go to the sender directly.
+  def initialize(msg)
+    @sender = IRC::MessageSender.new(STDOUT)
+    @nick = msg.nick
+    @public_lines = 0
+    @private_lines = 0
+    @default_dest = msg.reply_to if msg.respond_to?(:reply_to)
+  end
+
+  # Sends a PRIVMSG to the default destination with the given text.
+  def reply(text)
+    increment
+    @sender.privmsg(@default_dest, text)
+  end
+
+  # Send a PRIVMSG to the request message's nick with the given text.
+  def priv_reply(text)
+    increment_private
+    @sender.privmsg(@nick, text)
+  end
+
+  # Sends a /me action to the default destination with the given text.
+  def action(text)
+    increment
+    @sender.action(@default_dest, text)
+  end
+
+  # Sends the given raw irc message to the server.
+  def raw(msg)
+    increment
+    begin
+      @sender.send_msg(msg)
+    rescue ArgumentError => e
+      @sender.privmsg(@default_dest, e.message)
     end
+  end
 
-    # Sends a PRIVMSG to the default destination with the given text.
-    def reply(text)
-        increment
-        @@write_lock.synchronize do
-            @client.privmsg(@defdest, text)
-        end
+  private
+
+  def increment
+    if IRC.channel_name?(@default_dest)
+      increment_public
+    else
+      increment_private
     end
+  end
 
-    # Send a PRIVMSG to the request message's nick with the given text.
-    def priv_reply(text)
-        increment_private
-        @@write_lock.synchronize do
-            @client.privmsg(@msg.nick, text)
-        end
+  def increment_public
+    @public_lines += 1
+    if @public_lines > SOFT_PUBLIC_LIMIT
+      raise IRC::TooManyLines if @public_lines > SOFT_PUBLIC_LIMIT * 2
+      sleep 0.1
     end
+  end
 
-    # Sends a /me action to the default destination with the given text.
-    def action(text)
-        increment
-        @@write_lock.synchronize do
-            @client.action(@defdest, text)
-        end
+  def increment_private
+    @private_lines += 1
+    if @private_lines > SOFT_PRIVATE_LIMIT
+      raise IRC::TooManyLines if @private_lines > SOFT_PRIVATE_LIMIT * 2
+      sleep 0.1
     end
-
-    # Sends the given raw irc message to the server.
-    def raw(msg)
-        increment
-        @@write_lock.synchronize do
-            begin
-              @client.send_msg(msg)
-            rescue ArgumentError => e
-              @client.privmsg(@defdest, e.message)
-            end
-        end
-    end
-
-    # Holds the write lock on this replier for the duration of the given block.
-    def self.lock(&block)
-        @@write_lock = Mutex.new unless defined?(@@write_lock)
-        @@write_lock.synchronize do
-            block.call
-        end
-    end
-
-    private
-
-    def increment
-        if IRC.channel_name?(@defdest)
-            increment_public
-        else
-            increment_private
-        end
-    end
-
-    def increment_public
-        @public_lines += 1
-        if @public_lines > SOFT_PUBLIC_LIMIT
-            raise IRC::TooManyLines if @public_lines > SOFT_PUBLIC_LIMIT * 2
-            sleep 0.1
-        end
-    end
-
-    def increment_private
-        @private_lines += 1
-        if @private_lines > SOFT_PRIVATE_LIMIT
-            raise IRC::TooManyLines if @private_lines > SOFT_PRIVATE_LIMIT * 2
-            sleep 0.1
-        end
-    end
+  end
 end
