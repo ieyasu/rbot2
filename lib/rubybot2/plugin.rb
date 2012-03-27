ba98b9ae91ab4bade5803a98f550fad04e161fba
Matthew Bishop <mattsbishop@gmail.com>
2012-03-27 13:08:50 -0600

    catch exceptions from the message loop and report them

--- lib/rubybot2/plugin.rb
+++ lib/rubybot2/plugin.rb
@@ -12,18 +12,22 @@ def register(*messages)
   puts
 end
 
+# Given an exception, prints nicely formatted reporting information to STDERR.
+def report_exception(e, task = 'checking jobs')
+  bt = e.backtrace.inject('') { |s,t| "#{s}\n  #{t}" }
+  STDERR.puts "Caught exception #{e.class} #{task}: #{e.message} #{bt}"
+end
+
 # Wait for input, parse incoming messages, and yield with an IRC message
 # and replier.
 def message_loop
   while (line = STDIN.gets)
     msg = IRC.parse_message(line)
     replier = Replier.new msg
-    yield msg, replier
+    begin
+      yield msg, replier
+    rescue => e
+      report_exception e, 'from message loop block'
+    end
   end
-end
-
-# Given an exception, prints nicely formatted reporting information to STDERR.
-def report_exception(e)
-  bt = e.backtrace.inject('') { |s,t| "#{s}\n  #{t}" }
-  STDERR.puts "Caught exception #{e.class} checking jobs: #{e.message} #{bt}"
 end
