82dd3fc63e6f0117f3574771b41b0013c0dbe69e
Matthew Bishop <mattsbishop@gmail.com>
2012-03-27 21:32:20 -0600

    ported and fixed up for new imgur /r/ urls

---
+++ services/title_grabber.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# Grabs titles from web pages with non-useful URLs and displays them in
+# the originating channel. Just youtube for now.
+
+require 'rubybot2/plugin'
+require 'rubybot2/thread_janitor'
+require 'rubybot2/web'
+include Web
+
+GRAB_URLS = [%r!((?:https?://)?(?:\w+\.)?youtube\.com/watch[^ \t>)]+)!,
+             %r!((?:https?://)?imgur\.com/r/\w+/[\w\d]+)!i,
+             %r!((?:https?://)?(?:i\.)?imgur\.com/(?:gallery/)?[\w\d]+)!i]
+
+def fetch_title(url, replier)
+  body = read_url(url)
+  i = body.index('<title') or return
+  j = body.index('</title', i) or return
+  replier.reply("Title: #{strip_html(body[i...j])}")
+end
+
+# ---
+
+register IRC::CMD_PRIVMSG
+
+$janitor = ThreadJanitor.new
+
+message_loop do |msg, replier|
+  if GRAB_URLS.any? {|url| msg.text =~ url}
+    u = $1
+    $janitor.register(Thread.new { fetch_title(u, replier) })
+  end
+end
