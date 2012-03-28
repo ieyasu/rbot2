ef1125411c32dac594b76e8b8c4cb177c523cbda
Matthew Bishop <mattsbishop@gmail.com>
2012-03-27 21:40:59 -0600

    even more general imgur url matching

--- services/title_grabber.rb
+++ services/title_grabber.rb
@@ -8,8 +8,7 @@ require 'rubybot2/web'
 include Web
 
 GRAB_URLS = [%r!((?:https?://)?(?:\w+\.)?youtube\.com/watch[^ \t>)]+)!,
-             %r!((?:https?://)?imgur\.com/r/\w+/[\w\d]+)!i,
-             %r!((?:https?://)?(?:i\.)?imgur\.com/(?:gallery/)?[\w\d]+)!i]
+             %r!((?:https?://)?(?:i\.)?imgur\.com(?:/[\w\d]+)+)!i]
 
 def fetch_title(url, replier)
   body = read_url(url)
