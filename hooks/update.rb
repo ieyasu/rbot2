c9c3268a6c29a3c0ad0c2b68071fac95d7b7d9f8
Matthew Bishop <bishop@atmos.colostate.edu>
2012-04-01 22:31:32 -0600

    check if the service code has changed since the service was started.  If yes, reload all services.

--- hooks/update.rb
+++ hooks/update.rb
@@ -2,10 +2,23 @@
 
 SYNTAX = 'Usage: !update'
 
+def check_reload
+  if Dir['run/*.pid'].any? { |pid|
+      service = pid.gsub(/run\/(.*)\.pid/, "services/\\1.rb")
+      File.exist?(service) && File.mtime(service) > File.mtime(pid)
+    }
+    Process.kill('HUP', File.read('run/rubybot.pid').to_i)
+    "P\t*** Reloaded services\n"
+  else
+    ''
+  end
+end
+
 def handle_command(nick, dest, args)
   git = `git pull origin HEAD`
   out = ''
   git.each_line { |line| out << "P\t#{line.strip}\n" }
+  out << check_reload
   out
 end
 
