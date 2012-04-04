5377a2bbc31e706c063a85f3143d7f57fa7f30d9
Matthew Bishop <mattsbishop@gmail.com>
2012-04-03 20:41:32 -0600

    Use mt gox since tradehill seems to be gone

--- hooks/bitcoin.rb
+++ hooks/bitcoin.rb
@@ -4,14 +4,11 @@ require 'nokogiri'
 require 'json'
 
 def handle_command(nick, dest, args)
-  doc = Nokogiri::HTML(read_url("https://www.tradehill.com/MarketData/"))
-  data = doc.css("div#ticker2").xpath('table/tr/td').text
-  match = data.match(/Last: (\d+.\d+) USD, .+Highest Bid: (\d+.\d+)Lowest Ask: (\d+.\d+)/)
-  last, bid, ask = [1,2,3].map { |n| sprintf("$%0.2f", Float(match[n])) }
-
-  weighted = Float(JSON.parse(read_url("http://bitcoincharts.com/t/weighted_prices.json"))["USD"]["24h"])
-
-  "P\tTradeHill: #{last} -- bid: #{bid}, ask: #{ask}, 24h weighted: #{sprintf("$%0.2f", weighted)}"
+  doc = Nokogiri::HTML(read_url("https://mtgox.com"))
+  s = %w(lastPrice highPrice lowPrice volume weightedAverage).map do|id|
+    doc.css("li##{id}").text.gsub(':', ': ').gsub(/0+$/, '')
+  end.join(', ')
+  "P\tMt. Gox: #{s}"
 end
 
 load 'boilerplate.rb'
