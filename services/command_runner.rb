80c2ff38065f4ef18c692882188dd0d67f6ea9ec
Matthew Bishop <bishop@atmos.colostate.edu>
2012-04-11 23:40:16 -0600

    send math-looking messages to the calc command

--- services/command_runner.rb
+++ services/command_runner.rb
@@ -49,6 +49,8 @@ def run_hook(command, args, msg, replier)
     Open3.popen3(bin, msg.nick, msg.dest, args || '') do |_, out, err|
       process_hook_output(replier, out, err)
     end
+  elsif not msg.sent_to_channel? and command =~ /^\d/
+    run_hook('calc', msg.text, msg, replier) # looks like math
   else
     # XXX log non-existent hook
   end
@@ -91,6 +93,25 @@ def process_hook_output(r, out, err)
   end
 end
 
+def check_message(msg, replier)
+  return unless msg.text =~ /^(#{$rbconfig['cmd_prefix']})?([^\/\s]+)\s*(.*)$/
+  pfx,command,args = $1,$2,$3
+  command = command.downcase
+
+  # do not respond to public command words without a command prefix
+  return if msg.sent_to_channel? && !pfx
+
+  # ensure that commands which should be privately messaged
+  # only are in fact privately messaged
+  msg_only = $rbconfig['msg-commands'].member?(command)
+  if msg_only && msg.sent_to_channel?
+    r.priv_reply("Try: /msg #{$rbconfig['nick']} #{msg.text[1..-1]}")
+    return
+  end
+
+  Thread.new { run_hook(command, args, msg, replier) }
+end
+
 # ---
 
 register  IRC::CMD_PRIVMSG
@@ -98,22 +119,6 @@ register  IRC::CMD_PRIVMSG
 $plugins = {}
 load_plugins('plugins')
 $janitor = ThreadJanitor.new
-
 message_loop do |msg, replier|
-  next unless msg.text =~ /^(#{$rbconfig['cmd_prefix']})?([^\/\s]+)\s*(.*)$/
-  pfx,command,args = $1,$2,$3
-  command = command.downcase
-
-  # do not respond to public command words without a command prefix
-  next if msg.sent_to_channel? && !pfx
-
-  # ensure that commands which should be privately messaged
-  # only are in fact privately messaged
-  msg_only = $rbconfig['msg-commands'].member?(command)
-  if msg_only && msg.sent_to_channel?
-    r.priv_reply("Try: /msg #{$rbconfig['nick']} #{msg.text[1..-1]}")
-    next
-  end
-
-  $janitor.register(Thread.new { run_hook(command, args, msg, replier) })
+  t = check_message(msg, replier) and $janitor.register(t)
 end
