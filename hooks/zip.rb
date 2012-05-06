eeba591c0b8defba54e555c50bdd22028e58423c
Matthew Bishop <bishop@atmos.colostate.edu>
2012-05-06 02:29:34 -0600

    set TZ env var so we get the requested local time, not the account's local time

--- hooks/zip.rb
+++ hooks/zip.rb
@@ -3,6 +3,7 @@ include Zip
 
 zip = ($args.length > 0) ? $args : ENV['ZIP']
 zipinfo = get_zipinfo(zip)
+ENV['TZ'] = zipinfo.tz
 
 reply("Zipcode #{zip} is in #{zipinfo.city}, #{zipinfo.state}; lat,lon = #{zipinfo.lat}, #{zipinfo.lon}; timezone #{zipinfo.tz}")
 reply("Local time is #{Time.now.ctime}")
