1e05630a80b361fa948404e714def94a12fc9318
Matthew Bishop <bishop@atmos.colostate.edu>
2012-05-22 18:55:58 -0600

    say what has the highest and lowest points

--- hooks/==.rb
+++ hooks/==.rb
@@ -1,8 +1,8 @@
 m = match_args(/(.+)/) do
   top = DB[:points].order(:points.desc).limit(1).first
-  best = top ? top[:points] : ''
+  best = top ? "#{top[:thing]} = #{top[:points]}" : ''
   bot = DB[:points].order(:points).limit(1).first
-  worst = bot ? bot[:points] : ''
+  worst = bot ? "#{bot[:thing]} = #{bot[:points]}" : ''
   "<thing>; best: #{best}, worst: #{worst}"
 end
 what = m[1]
