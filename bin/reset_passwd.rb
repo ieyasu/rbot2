6bed18ed39f8431d686628d9beaf5fce9e9df2e6
Matthew Bishop <bishop@atmos.colostate.edu>
2012-05-25 22:38:24 -0600

    it actually works this time, I promise\!

--- bin/reset_passwd.rb
+++ bin/reset_passwd.rb
@@ -4,21 +4,12 @@ require 'rubygems'
 require 'digest/md5'
 load 'config.rb'
 require 'rubybot2/db'
-
-# Returns the salted, hashed password ready to store in the db
-def hash_passwd(pass)
-    salt = ''
-    File.open('/dev/urandom') do |fin|
-        6.times { salt << sprintf("%x", fin.getc) }
-    end
-    "#{salt}:#{MD5.new(salt + pass).hexdigest}"
-end
+require 'rubybot2/account'
 
 unless ARGV.length == 2
     puts "Usage: account newpass"
     exit 1
 end
 
-account = Account.ds_by_nick(ARGV[0])
-newpass = ARGV[1]
-account.update(:passwd => hash_passwd(newpass))
+account, newpass = ARGV
+puts DB[:accounts].filter(:name => account).update(:passwd => Account::hash_passwd(newpass))
