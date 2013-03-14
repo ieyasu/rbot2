63965e3151a7067b53d87e44b5d44296955831e8
Aaron Zink <azink@solsar.com>
2013-03-14 10:26:24 -0700

    fixing stock

--- hooks/stock.rb
+++ hooks/stock.rb
@@ -1,41 +1,22 @@
-def parse_quote(body, symbol)
-  i = body.index('Last Price') or return
-  i = body.index('<td', i + 14) or return
-  j = body.index('</td', i) or return
-  price = strip_html(body[i...j])
-  #p price
-
-  if price =~ /N\/A/
-    return "Stock symbol #{symbol} not found"
+def parse_quote(body)
+  require "CSV"
+  CSV.parse(body).first do |s|
+    symbol = s[0]
+    price = s[1]
+    date = s[2]
+    time = s[3]
+    change = s[4]
+    changepc = s[5]
+    name = s[6]
   end
-
-  i = body.index('Change</td>') or return
-  i = body.index('<td', i + 11) or return
-  j = body.index("</td", i) or return
-  change = strip_html(body[i...j])
-  change = '+' + change if change[0..0] != '-'
-  #p change
-
-  i = body.index('% Change</td>') or return
-  i = body.index('<td', i + 13) or return
-  j = body.index("</td", i) or return
-  changepc = strip_html(body[i...j])
-  changepc = '+' + changepc if change[0..0] != '-'
-  changepc = "(#{changepc})"
-  #p changepc
-
-  i = body.index(/Last (?:Trade|Update)<\/td/) or return
-  i = body.index('<td', i + 12) or return
-  j = body.index('</td', i) or return
-  time = strip_html(body[i...j])
-  #p time
-
-  "#{symbol.upcase}: #{price}  #{change} #{changepc}  closed #{time}"
+  "#{name} (#{symbol.upcase}): #{price} #{change} #{changepc} last trade #{date} #{time}"
 end
 
 match_args /\S+/, '<symbol>'
 
-body = http_get("http://mobile.quote.com/quotes.aspx?symbol=#", $args)
+body = http_get("http://finance.yahoo.com/d/quotes.csv?s=", $args + "&f=sl1d1t1c1p2n")
+#symbol,price,date,time,change,pct,name
+#"GOOG", "820.34", "3/14/2013", "1:02pm", "-4.97", "-0.60%", "Google Inc."] 
 
 if (quote = parse_quote(body, $args))
     reply quote
