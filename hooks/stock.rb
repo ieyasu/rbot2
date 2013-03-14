b4405f332845b133b299e7d864106141f627593d
Aaron Zink <azink@solsar.com>
2013-03-14 10:30:48 -0700

    fixing stock

--- hooks/stock.rb
+++ hooks/stock.rb
@@ -7,9 +7,9 @@ def parse_quote(body)
     time = s[3]
     change = s[4]
     changepc = s[5]
-    name = s[6]
+    stockname = s[6]
   end
-  "#{name} (#{symbol.upcase}): #{price} #{change} #{changepc} last trade #{date} #{time}"
+  "#{stockname} (#{symbol.upcase}): #{price} #{change} #{changepc} last trade #{date} #{time}"
 end
 
 match_args /\S+/, '<symbol>'
