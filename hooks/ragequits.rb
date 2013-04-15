affd4776ab6506e1f598db67be8aac01ca6b4361
Aaron Zink <azink@solsar.com>
2013-04-15 13:40:57 -0700

    updated ragequits

--- hooks/ragequits.rb
+++ hooks/ragequits.rb
@@ -3,6 +3,5 @@ require 'date'
 ragequits = Hash.new
 
 ragequits['bishop'] = Date.new(2013, 2, 8)
-ragequits['mike'] = Date.new(2013, 4, 8)
 
 ragequits.each {|k,v| reply "#{k} ragequit on #{v} (#{(Date.today - v).to_i.abs} days ago)" }
