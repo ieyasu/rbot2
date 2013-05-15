8d370707ba51ef43f2d07bdd0db062f4dc57b57c
Aaron Zink <azink@solsar.com>
2013-05-15 08:33:02 -0700

    changing bitcoin to use api

--- hooks/bitcoin.rb
+++ hooks/bitcoin.rb
@@ -1,5 +1,4 @@
-doc = noko_get("https://mtgox.com")
-s = %w(lastPrice highPrice lowPrice volume weightedAverage).map do|id|
-  doc.css("li##{id}").text.gsub(':', ': ').gsub(/0+$/, '')
-end.join(', ')
-reply "Mt. Gox: #{s}"
+require 'json'
+body = http_get("http://data.mtgox.com/api/2/BTCUSD/money/ticker", $args)
+d = JSON.parse(body)['data']
+reply "Mt Gox: Last: #{d['last']['display']}, High: #{d['high']['display']}, Low: #{d['low']['display']}, Volume: #{d['vol']['display'].sub("\u00A0BTC", "")}, Weighted Avg: #{d['avg']['display']}"
