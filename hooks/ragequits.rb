8b368699d507b7f07a6d19890e40e1f1d6541d87
Aaron Zink <azink@solsar.com>
2013-06-05 09:45:59 -0700

    updated ragequits

--- hooks/ragequits.rb
+++ hooks/ragequits.rb
@@ -3,6 +3,6 @@ require 'date'
 ragequits = Array.new
 
 ragequits.push(:name => 'galen', :date => Date.new(2012, 5, 7), :quote => "A sandy vag is never good.")
-ragequits.push(:name => 'bishop', :date => Date.new(2013, 2, 8), :quote => "and fuck off with that little man shit")
+ragequits.push(:name => 'r0s', :date => Date.new(2013, 5, 19), :quote => "J-Roc: fuck you")
 
 ragequits.each {|r| reply "#{r[:name]} ragequit on #{r[:date]} (#{(Date.today - r[:date]).to_i.abs} days ago): #{r[:quote]}"}
