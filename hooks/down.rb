9042dd88192e6aa3ff6f183eb52c518b2dd1aaa9
Matthew Bishop <mattsbishop@gmail.com>
2013-08-05 13:42:56 -0600

    silly error message

--- hooks/down.rb
+++ hooks/down.rb
@@ -1,4 +1,8 @@
-m = match_args /^(?:[^\.]+\.)+[^\.]+$/, '<domain.tld>'
-doc = noko_get("http://www.isup.me/#{$args}")
-resp = doc.css('#container').text
-reply resp.gsub(/Check another.*/m, '').strip.squeeze(' ')
+begin
+  m = match_args /^(?:[^\.]+\.)+[^\.]+$/, '<domain.tld>'
+  doc = noko_get("http://www.isup.me/#{$args}")
+  resp = doc.css('#container').text
+  reply resp.gsub(/Check another.*/m, '').strip.squeeze(' ')
+rescue
+  reply "trees are down!"
+end
