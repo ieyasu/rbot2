5ef9955fbbf93e21b56daff66e09204a2eff9730
Aaron Zink <azink@solsar.com>
2013-08-30 10:59:04 -0700

    fixing news.rb

--- hooks/news.rb
+++ hooks/news.rb
@@ -1,7 +1,8 @@
 require 'rss'
+require 'shorturl'
 rss = RSS::Parser.parse("http://news.google.com/news/feeds?q=#{$args}&output=rss", false)
 if rss && rss.channel.items.count > 0 then
-  reply rss.channel.items.first.title
+  reply "#{rss.channel.items.first.title} (#{ShortURL.shorten(rss.channel.items.first.link)})"
 else
   reply "No news found.  No news is good news?"
 end
