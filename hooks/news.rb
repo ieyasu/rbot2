9a6f4dcc111ddc4148f4be89b9a3057c654740df
Aaron Zink <azink@solsar.com>
2013-08-30 10:44:53 -0700

    adding news.rb

--- hooks/news.rb
+++ hooks/news.rb
@@ -1,6 +1,6 @@
 require 'rss'
 rss = RSS::Parser.parse("http://news.google.com/news/feeds?q=#{$args}&output=rss", false)
-if rss.feed_type == 'rss' then
+if rss && rss.channel.items.count > 0 then
   reply rss.channel.items.first.title
 else
   reply "No news found.  No news is good news?"
