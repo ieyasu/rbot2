eda9421e227df26de60d13418d4648a1744ca7f3
Aaron Zink <azink@solsar.com>
2013-11-10 15:05:53 -0800

    Adding simple output when no input interpretation is available

--- hooks/ask.rb
+++ hooks/ask.rb
@@ -2,8 +2,10 @@ require 'wolfram'
 
 Wolfram.appid = "L6LWRQ-5JY85G3LQ5"
 r = Wolfram::HashPresenter.new(Wolfram.fetch($args)).to_hash
-if r[:pods]['Result'] then
+if r[:pods]['Result'] and r[:pods]['Input interpretation'] then
   reply("Result: #{r[:pods]['Result'].first} (Interpreted input: #{r[:pods]['Input interpretation'].first})")
+elsif r[:pods]['Result']
+  reply("Result: #{r[:pods]['Result'].first}")
 else
   reply("THERE ARE FOUR LIGHTS")
 end
