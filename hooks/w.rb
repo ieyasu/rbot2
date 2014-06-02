b40ac7001a541121c67fab838f5b546807ffb2a9
Aaron Zink <azink@solsar.com>
2014-06-02 16:50:36 -0700

    switching w to wunderground

--- hooks/w.rb
+++ hooks/w.rb
@@ -5,7 +5,7 @@ w = Wunderground.new($rbconfig['wunderground_key'])
 
 $args = ENV['ZIP'] if $args.length == 0
 
-wx = w.conditions_for($args)['current_observations']
+wx = w.conditions_for($args)['current_observation']
 if wx then
   out = "#{wx['display_location']['full']} #{wx['observation_time'][/, (.+$)/, 1]}: #{wx['weather']} #{wx['temp_f']}F, #{wx['relative_humidity']}, wind #{wx['wind_dir']} at #{wx['wind_mph']} mph"
   out += " gust #{wx['wind_gust_mph']}" if wx['wind_gust_mph'].to_f > 0
