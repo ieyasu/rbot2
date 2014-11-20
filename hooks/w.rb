3fa0460fdb730578e0c31c7dc83e82ace8494a4a
Aaron Zink <azink@solsar.com>
2014-11-19 17:58:53 -0800

    Adding WX observation location to output

--- hooks/w.rb
+++ hooks/w.rb
@@ -7,7 +7,7 @@ $args = ENV['ZIP'] if $args.length == 0
 
 wx = w.conditions_for($args)['current_observation']
 if wx then
-  out = "#{wx['display_location']['full']} #{wx['observation_time'][/, (.+$)/, 1]}: #{wx['weather']} #{wx['temp_f']}F, #{wx['relative_humidity']}, wind #{wx['wind_dir']} at #{wx['wind_mph']} mph"
+  out = "#{wx['observation_location']['city']}, #{wx['display_location']['state']} #{wx['observation_time'][/, (.+$)/, 1]}: #{wx['weather']} #{wx['temp_f']}F, #{wx['relative_humidity']}, wind #{wx['wind_dir']} at #{wx['wind_mph']} mph"
   out += " gust #{wx['wind_gust_mph']}" if wx['wind_gust_mph'].to_f > 0
   out += ", precip today: #{wx['precip_today_in']}" if wx['precip_today_in'].to_f > 0
   reply(out)
